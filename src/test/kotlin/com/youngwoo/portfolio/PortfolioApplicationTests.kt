b2a8e5a51a1531db0691687005bd38297ab8ffa3
youngwooKim <rladyddn258@gmail.com>
2025-10-03 11:45:54 +0900

    Git과 Github

---
+++ src/test/kotlin/com/youngwoo/portfolio/PortfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.youngwoo.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
