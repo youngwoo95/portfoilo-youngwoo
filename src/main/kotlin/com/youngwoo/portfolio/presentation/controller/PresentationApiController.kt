e83db45a2570fa6659b808f6b0642c8b4b84971a
youngwooKim <rladyddn258@gmail.com>
2025-10-05 19:39:44 +0900

    컨트롤러 개발

--- src/main/kotlin/com/youngwoo/portfolio/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/com/youngwoo/portfolio/presentation/controller/PresentationApiController.kt
@@ -1,15 +1,44 @@
 package com.youngwoo.portfolio.presentation.controller
 
+import com.youngwoo.portfolio.presentation.dto.IntroductionDTO
+import com.youngwoo.portfolio.presentation.dto.LinkDTO
+import com.youngwoo.portfolio.presentation.dto.ProjectDTO
+import com.youngwoo.portfolio.presentation.dto.ResumeDTO
+import com.youngwoo.portfolio.presentation.service.PresentationService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class PresentationApiController {
+class PresentationApiController(
+    private val presentationService: PresentationService
+) {
+
 
     @GetMapping("/test")
     fun test():String{
         return "OK"
     }
+
+    @GetMapping("/v1/introudctions")
+    fun getIntroductions():List<IntroductionDTO>{
+        return presentationService.getIntroductions()
+    }
+
+    @GetMapping("/v1/links")
+    fun getLinks():List<LinkDTO>{
+        return presentationService.getLinks()
+    }
+
+    @GetMapping("/v1/resume")
+    fun getResume(): ResumeDTO{
+        return presentationService.getResume()
+    }
+
+    @GetMapping("/v1/projects")
+    fun getProjects():List<ProjectDTO>{
+        return presentationService.getProjects()
+    }
+
 }
